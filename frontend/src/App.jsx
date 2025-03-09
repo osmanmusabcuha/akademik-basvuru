c8a59ab5348558f76f87c129945b8e88ae36055d
AlperenGrmz <alperen.41gs@hotmail.com>
2025-03-09 17:35:20 +0300

    zustand yüklendi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "./components/ui/button";
+import { useStore } from "../store/store";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useStore((state) => state.count);
+  const increment = useStore((state) => state.increment);
+  const decrement = useStore((state) => state.decrement);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mx-auto mt-10 border border-gray-200 rounded-md shadow-md w-[400px]">
+      <div className="container mx-auto p-4 space-y-5">
+        <h1 className="text-4xl text-center">Counter</h1>
+        <div className="flex justify-center space-x-4">
+          <Button
+            className="border border-green-500 hover:bg-green-500"
+            onClick={increment}
+          >
+            Click me
+          </Button>
+          <Button
+            className="border border-red-500 hover:bg-red-500"
+            onClick={decrement}
+          >
+            Click me
+          </Button>
+        </div>
+        <div className="text-center text-3xl">{count}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
