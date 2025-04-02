e1c53e7d2229710f7f34c4080e53f7a4cc066d77
AlperenGrmz <alperen.41gs@hotmail.com>
2025-04-02 23:24:26 +0300

    basvurular tablosu eklendi

---
+++ backend/src/db/schema/application.js
@@ -0,0 +1,27 @@
+import { pgTable, integer, varchar, date } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { users } from "./users.js";
+import { posting } from "./posting.js";
+
+export const applications = pgTable("applications", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer("user_id")
+    .references(() => users.id)
+    .notNull(),
+  postingId: integer("posting_id")
+    .references(() => posting.id)
+    .notNull(),
+  status: varchar("status", { length: 255 }).notNull(),
+  applicationDate: date("application_date").notNull(),
+});
+
+export const applicationsRelations = relations(applications, ({ one }) => ({
+  user: one(users, {
+    fields: [applications.userId],
+    references: [users.id],
+  }),
+  posting: one(posting, {
+    fields: [applications.postingId],
+    references: [posting.id],
+  }),
+}));
