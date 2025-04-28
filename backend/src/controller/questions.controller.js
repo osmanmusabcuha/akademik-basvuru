4bc0813871d44896ab8b5d1b6bb8f4a23a49d320
AlperenGrmz <alperen.41gs@hotmail.com>
2025-04-28 03:42:06 +0300

    question controller düzenlendi

--- backend/src/controller/questions.controller.js
+++ backend/src/controller/questions.controller.js
@@ -4,30 +4,11 @@ import { applicationQuestions } from "../db/schema/applicationQuestions.js";
 
 export const getAllQuestions = async (req, res) => {
   try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 20;
+    const questions = await db.select().from(applicationQuestions);
 
-    const questions = await db
-      .select()
-      .from(applicationQuestions)
-      .limit(limit)
-      .offset((page - 1) * limit);
-
-    const totalQuestions = await db
-      .select({ count: count() })
-      .from(applicationQuestions);
-
-    res.status(200).json({
-      questions,
-      pagination: {
-        total: Number(totalQuestions[0].count),
-        page,
-        limit,
-        totalPages: Math.ceil(Number(totalQuestions[0].count) / limit),
-      },
-    });
+    res.status(200).json({ questions });
   } catch (error) {
-    console.error("Error fetching questions:", error);
+    console.error("Error fetching all questions:", error);
     res
       .status(500)
       .json({ message: "Internal server error", error: error.message });
