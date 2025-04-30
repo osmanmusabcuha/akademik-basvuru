5fd0fb25316a8080d41c27d8dbd3a0fcfdbcbfe9
AlperenGrmz <alperen.41gs@hotmail.com>
2025-04-30 03:17:07 +0300

    answers routendaki hata duzeltildi

--- backend/src/routes/answers.routes.js
+++ backend/src/routes/answers.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   uploadAnswer,
   getAnswersByApplicationId,
-  putScoreApplicationById,
+  putScoreById,
 } from "../controller/answers.controller.js";
 import { uploadSingle } from "../middleware/upload.middleware.js";
 
@@ -10,6 +10,6 @@ const answersRouter = express.Router();
 
 answersRouter.post("/upload", uploadSingle("file"), uploadAnswer);
 answersRouter.get("/application/:applicationId", getAnswersByApplicationId);
-answersRouter.put("/application/:applicationId/score", putScoreApplicationById);
+answersRouter.put("/application/:applicationId/score", putScoreById);
 
 export default answersRouter;
